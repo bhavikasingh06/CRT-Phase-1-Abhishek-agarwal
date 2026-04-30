47f58dd0e4d0c6abf16db577598b429a4d560703
Bhavika Singh <bhavikasingh6666@gmail.com>
2026-04-30 21:15:08 +0530

    program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,17 @@
+//program 1
+//to convert celcius to fahranite
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double celcius;
+    double fahranite;
+    cin>>celcius;
+    
+    fahranite = (celcius*9.0/5.0)+32;
+    cout<<fahranite<<endl;
+
+    return 0;
+}
