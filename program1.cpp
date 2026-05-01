76e8fe50874812b3f75516f57ac01860be7c8649
Bhavika Singh <bhavikasingh6666@gmail.com>
2026-05-01 13:51:37 +0530

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -15,3 +15,48 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+//program 2
+//local and global variable
+
+#include <iostream>
+using namespace std;
+int globalvariable = 100;
+
+int main()
+{
+    int localvariable;
+    int result;
+    cin>>localvariable;
+    
+    result = (globalvariable/localvariable);
+    cout<<result<<endl;
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
