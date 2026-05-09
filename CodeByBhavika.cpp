3c885e8a2bc6635162cdc5388cd6c94c12b7cbf2
Bhavika Singh <bhavikasingh6666@gmail.com>
2026-05-09 22:32:01 +0530

    Update CodeByBhavika.cpp

--- CodeByBhavika.cpp
+++ CodeByBhavika.cpp
@@ -217,9 +217,30 @@ int main() {
 
 
 
+//INHERITANCE
+//7
+// 1 single inheritance
+#include <iostream>
+using namespace std;
+class Animal{
+public:
+    void eat(){
+        cout<<"animal eats food..."<<endl;
+    }
+};
 
+class Dog : public Animal {
+public:
+    void bark(){
+        cout<<"dog bark.."<<endl;
+    }
+};
 
-
+int main(){
+    Dog d;
+    d.eat();
+    d.bark();
+}
 
 
 
