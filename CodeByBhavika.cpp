d7b7d83586b7ef5dd13dc89169094df146cd3eda
Bhavika Singh <bhavikasingh6666@gmail.com>
2026-05-17 20:53:50 +0530

    CodeByBhavika.cpp

--- CodeByBhavika.cpp
+++ CodeByBhavika.cpp
@@ -1267,4 +1267,41 @@ int main(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+//28
+//number is prime or not 
+#include<iostream>
+using namespace std;
+int main(){
+    int n, count = 0;
+    cout<<"enter a number = ";
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+        count++;
+        }
+    }
+    if(count == 2){
+        cout<<"prime";
+    }else{
+        cout<<"not prime";
+    }
+    return 0;
+}
+
+
+
+
     
